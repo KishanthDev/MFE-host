8e699767df54aecc2d05ab26be94f31779801dca
KishanthDev <kishanthkisna@gmail.com>
2026-04-03 12:24:37 +0530

    overflow

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,9 +2,9 @@ import Sidebar from "./Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex">
+    <div className="flex h-screen overflow-hidden">
       <Sidebar />
-      <main className="flex-1 bg-gray-100 min-h-screen p-6">
+      <main className="flex-1 bg-gray-100 p-6 overflow-y-auto">
         {children}
       </main>
     </div>
